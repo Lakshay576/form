449d92d8f7b494ee66bce47fbe379c5b9fcbf06c
Lakshay <lakshaybankura393@gmail.com>
2025-09-08 15:35:00 +0530

    Add deploy link

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -76,7 +76,7 @@ const Login = () => {
     e.preventDefault();
     if (validate()) {
         try {
-          const res = await fetch("http://localhost:5000/api/signUp",{
+          const res = await fetch("https://form-j7du.onrender.com/api/signUp",{
             method: 'POST',
             headers: {'Content-Type':'application/json'},
             body: JSON.stringify(formData),
--- server/app.js
+++ server/app.js
@@ -3,10 +3,7 @@ const app = express();
 const authroutes = require('./routes/auth')
 const cors = require('cors');
 
-app.use(cors({
-    origin: "http://localhost:5173",
-    credentials: true,
-}))
+app.use(cors());
 
 app.use(express.json());
 app.use('/api', authroutes)
